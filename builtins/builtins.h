88b369ad967e473f3cf459c5e3c685dd202383a3
Peter Vass <pvass@c4r6p8.42vienna.com>
2024-09-30 15:56:57 +0200

    small chganges

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -6,7 +6,7 @@
 /*   By: pvass <pvass@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 12:28:57 by icseri            #+#    #+#             */
-/*   Updated: 2024/09/30 15:49:56 by pvass            ###   ########.fr       */
+/*   Updated: 2024/09/30 15:55:01 by pvass            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ bool	replace_var(t_var *data, char *key, char *content);
 bool	is_valid_var(t_var *data, char *line);
 void	no_arg_export(t_var *data);
 void	ex_util2(t_var *data);
+void	bubble_sort_with_context(int *arr, int n, char **env);
+void	print_export(char *line);
 
 char	*get_word(char *text, char *separator);
 
