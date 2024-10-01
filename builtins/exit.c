b29d16500798e579f7b870b77218bb58a8092d06
Peter Vass <pvass@c4r6p6.42vienna.com>
2024-10-01 15:39:31 +0200

    ex

--- builtins/exit.c
+++ builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icseri <icseri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: pvass <pvass@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 10:52:52 by icseri            #+#    #+#             */
-/*   Updated: 2024/09/30 17:01:26 by icseri           ###   ########.fr       */
+/*   Updated: 2024/10/01 15:31:31 by pvass            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	ft_exit(t_var *data)
 	ft_putendl_fd("exit", STDERR_FILENO);
 	if (data->cmd_list[1] && data->cmd_list[2])
 	{
-		print_error(3, "minishell: exit: too many arguments");
+		print_error(1, "minishell: exit: too many arguments");
 		data->exit_code = ERROR_MISUSE;
 		return ;
 	}
